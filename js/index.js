88898196f26665f3f90b21d225465e7c42d67331
justinliao122 <liaoju@bu.edu>
2024-11-11 00:48:04 -0500

    could get git to add my video file so had to wait for youtube to upload it for assignment 7

---
+++ js/index.js
@@ -0,0 +1,40 @@
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Script loaded and running on the page');
+    const glow = document.createElement('div');
+    glow.classList.add('glowing-effect');
+    document.body.appendChild(glow);
+
+    document.addEventListener('mousemove', (e) => {
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+        glow.style.left = mouseX + 'px';
+        glow.style.top = mouseY + 'px';
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var aboutMeBody = document.querySelector('.aboutme__body--aboutme');
+
+  // Enable scrolling on hover
+  aboutMeBody.addEventListener('mouseenter', function() {
+    aboutMeBody.style.overflowY = 'auto';
+  });
+
+  // Disable scrolling when not hovering
+  aboutMeBody.addEventListener('mouseleave', function() {
+    aboutMeBody.style.overflowY = 'hidden';
+  });
+});
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const scrollContainer = document.querySelector('.hobbies-scrollable');
+  scrollContainer.scrollLeft = 0; // Explicitly set the scroll position to the start
+});
+
+
+
